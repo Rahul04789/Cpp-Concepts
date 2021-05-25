a03e7462b6579d5af7f1ae2aff4bb20624228868
Rahul04789 <rahulkumar04789@gmail.cm>
2021-05-25 21:13:30 +0530

    Added MyString implementation

---
+++ MyString_implementation.cpp
@@ -0,0 +1,49 @@
+class MyString
+{
+    int m_length;
+    char * m_string;
+public:
+    MyString():m_length(0),m_string(nullptr){}
+    MyString(const char * rhs):m_length(0),m_string(rhs){}
+    MyString(const MyString& rhs);
+    MyString& operator= (const MyString& rhs);
+};
+MyString& MyString::operator= (const MyString& rhs)
+{
+    if(&rhs == this)
+        return *this;
+    int l=m_length;
+    char* pc=m_string;
+    m_string=nullptr;
+    m_length=rhs.m_length;
+    m_string=new (std::no_throw) char[m_length];
+    if(m_string != nullptr)
+    {
+        strcpy(rhs.m_string, m_string, m_length);
+        if(pc!= nullptr)
+            delete []pc;
+    }
+    else
+    {
+        m_length = l;
+        m_string = pc;
+    }
+    return *this;
+}
+
+MyString::MyString(const MyString& rhs)
+{
+    m_string=nullptr;
+    m_length=rhs.m_length;
+    m_string=new (std::no_throw) char[m_length];
+    if(m_string != nullptr)
+    {
+        strcpy(rhs.m_string, m_string, m_length);
+    }
+    else
+    {
+        m_length = 0;
+        m_string = nullptr;
+    }
+}
+
